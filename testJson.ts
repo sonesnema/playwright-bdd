428b4ddb6c6c1423be0425c419d13c29c5a8ae79
@soneshnema <nemasonesh121@gmail.com>
2024-12-20 22:44:41 +0530

    secretTest

--- testJson.ts
+++ testJson.ts
@@ -1,9 +1,8 @@
 const crypt=require('crypto-js')
-var key=process.env.npm_config_env
 
 var ciphertext = 'U2FsdGVkX1/2QXUsFJ0D0CPzsa0D6h8JZ0VunTnezGQ='
 // Decrypt
-var bytes  = crypt.AES.decrypt(ciphertext,key);
+var bytes  = crypt.AES.decrypt(ciphertext,process.env.npm_config_env);
 var originalText = bytes.toString(crypt.enc.Utf8);
 
 console.log(originalText); // 'my message'
