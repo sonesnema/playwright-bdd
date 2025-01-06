330740451fc78611f965bcaa54a70d5020891bdd
@soneshnema <nemasonesh121@gmail.com>
2025-01-06 12:54:54 +0530

    secret

--- testJson.ts
+++ testJson.ts
@@ -1,8 +1,10 @@
 const crypt=require('crypto-js')
 
-var ciphertext = 'U2FsdGVkX1/2QXUsFJ0D0CPzsa0D6h8JZ0VunTnezGQ='
+var ciphertext = 'U2FsdGVkX18PF2nlKZLhDOV7o83oC/3IekZmb4X0hzs='
 // Decrypt
 var bytes  = crypt.AES.decrypt(ciphertext,process.env.npm_config_env);
 var originalText = bytes.toString(crypt.enc.Utf8);
+// var enc  = crypt.AES.encrypt("test",process.env.npm_config_env).toString();
+// console.log(enc)
 
 console.log(originalText); // 'my message'
